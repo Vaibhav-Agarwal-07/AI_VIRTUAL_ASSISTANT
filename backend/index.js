8b3b685a216ecdd92648c0820febcecb1347fc79
VAIBHAV-AGARWAL-07 <Vaibhav142003@gmail.com>
2025-11-20 15:24:43 +0530

    Fix CORS + final backend deployment setup

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
@@ -6,23 +7,26 @@ import authRouter from "./routes/auth.routes.js"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import userRouter from "./routes/user.routes.js"
-import geminiResponse from "./gemini.js"
 
+const app = express()
 
-const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: [
+        "http://localhost:5173",
+        "https://your-netlify-name.netlify.app"
+    ],
+    credentials: true
 }))
-const port=process.env.PORT || 5000
+
+const port = process.env.PORT || 5000
+
 app.use(express.json())
-app.use(cookieParser())
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
+app.use(cookieParser())  
 
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
 
-app.listen(port,()=>{
+app.listen(port, () => {
     connectDb()
     console.log("server started")
-})
-
+})  
